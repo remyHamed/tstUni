11e6098c9cfd32667bd4c11e9a01d719b87ae509
hemarydem <43240309+hemarydem@users.noreply.github.com>
2022-03-29 12:48:07 +0200

    ajout fichier testé

---
+++ CalTest.ts
@@ -0,0 +1,12 @@
+import { Cal } from "./cal";
+
+
+
+    describe("Foo", function () {
+        describe("#clone", function () {
+
+            it("clones the object", function () {
+            });
+        });
+    });
+
