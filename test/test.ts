e01e34f36ecaadab2c7774c26a8ae601a4e2e3ef
Remy Hamed <remyhamed@MacBook-de-Remy.local>
2022-05-23 13:00:07 +0200

    new test on user missing mock test

--- test/test.ts
+++ test/test.ts
@@ -1,17 +1,22 @@
 import {User } from '../src/Class/User'; // this will be your custom import
 import { expect } from 'chai';
 
-describe('Options tests', () => { // the tests container
-    it('checking default options', () => { // the single test
+describe('user tests', () => { // the tests container
+    it('check emptY firstname', () => { // the single test
         const u = new User("", "toto","remu@hotmail.fr",new Date(0)); // this will be your class
-
-                /* detect retina */
         expect(u.issetFullName(u.firstName)).to.be.false; // Do I need to explain anything? It's like writing in English!
+    });
 
-        /* fps limit */
-       // expect(options.fpsLimit).to.equal(30); // As I said 3 lines above
-
-        expect(u.firstName).to.be.empty; // emitters property is an array and for this test must be empty, this syntax works with strings too
-       // expect(options.particles.color).to.be.an("object").to.have.property("value").to.equal("#fff"); // this is a little more complex, but still really clear
+    it('check ok last name', () => { // the single test
+        const u = new User("", "toto","remu@hotmail.fr",new Date(0)); // this will be your class
+        expect(u.issetFullName(u.lastName)).to.be.true; // Do I need to explain anything? It's like writing in English!
     });
 });
+/*`
+describe('Options tests', () => { // the tests container
+  it('checking default options', () => { // the single test
+      const u2 = new User("titi", "toto","remuotmail.fr",new Date(0)); // this will be your class
+      expect(u2.firstName).to.be.empty; // emitters property is an array and for this test must be empty, this syntax works with strings too
+  });
+});
+*/
